f849ad807443b38272b19304ea260294e2f3f05d
Bry Cox <carl.cc@gmail.com>
2019-05-07 13:22:51 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,35 @@ INSERT INTO books VALUES (3, "Ellipse_2", 3, 5);
 INSERT INTO books VALUES (4, "Ellipse_3", 3, 5);
 INSERT INTO books VALUES (5, "Ellipse_4", 3, 5);
 INSERT INTO books VALUES (6, "Ellipse_5", 3, 5);
+
+INSERT INTO subgenres VALUES (1, "With");
+INSERT INTO subgenres VALUES (2, "Without");
+
+INSERT INTO authors VALUES (1, "Volde");
+INSERT INTO authors VALUES (2, "Odin");
+
+INSERT INTO character_books VALUES (1, 2, 4);
+INSERT INTO character_books VALUES (2, 2, 4);
+INSERT INTO character_books VALUES (3, 2, 4);
+INSERT INTO character_books VALUES (4, 2, 4);
+INSERT INTO character_books VALUES (5, 3, 6);
+INSERT INTO character_books VALUES (6, 3, 6);
+INSERT INTO character_books VALUES (7, 1, 3);
+INSERT INTO character_books VALUES (8, 4, 2);
+INSERT INTO character_books VALUES (9, 2, 4);
+INSERT INTO character_books VALUES (10, 2, 4);
+INSERT INTO character_books VALUES (11, 2, 4);
+INSERT INTO character_books VALUES (12, 2, 4);
+INSERT INTO character_books VALUES (13, 3, 6);
+INSERT INTO character_books VALUES (14, 3, 6);
+INSERT INTO character_books VALUES (15, 1, 3);
+INSERT INTO character_books VALUES (16, 4, 2);
+
+INSERT INTO characters VALUES (1, "Volde", "Newt", "Pastel_1", 4, 8);
+INSERT INTO characters VALUES (2, "Volde", "Newt", "Pastel_1", 2, 8);
+INSERT INTO characters VALUES (3, "Volde", "Newt", "Pastel_1", 3, 8);
+INSERT INTO characters VALUES (4, "Volde", "Newt", "Pastel_1", 4, 8);
+INSERT INTO characters VALUES (5, "Volde", "Newt", "Pastel_1", 5, 8);
+INSERT INTO characters VALUES (6, "Volde", "Newt", "Pastel_1", 4, 8);
+INSERT INTO characters VALUES (7, "Volde", "Newt", "Pastel_1", 1, 8);
+INSERT INTO characters VALUES (8, "Volde", "Newt", "Pastel_1", 3, 8);
