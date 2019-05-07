49e1851d7d0937800053e0e48cff17441896d725
Bry Cox <carl.cc@gmail.com>
2019-05-07 11:22:21 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO series VALUES (1, "New Title", 2, 8);
+INSERT INTO series VALUES (2, "No Title", 3, 5);
+
+INSERT INTO books VALUES (1, "Ellipse", 3, 5);
+INSERT INTO books VALUES (2, "Ellipse_1", 3, 5);
+INSERT INTO books VALUES (3, "Ellipse_2", 3, 5);
+INSERT INTO books VALUES (4, "Ellipse_3", 3, 5);
+INSERT INTO books VALUES (5, "Ellipse_4", 3, 5);
+INSERT INTO books VALUES (6, "Ellipse_5", 3, 5);
